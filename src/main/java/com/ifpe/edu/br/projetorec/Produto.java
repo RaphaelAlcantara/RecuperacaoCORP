9bc3d4319a6c896b10670d66e3ffc8711911fec5
1angelomig <angelomig13@outlook.com>
2022-12-25 23:44:36 -0300

    discriminador do produto arrumado

--- src/main/java/com/ifpe/edu/br/projetorec/Produto.java
+++ src/main/java/com/ifpe/edu/br/projetorec/Produto.java
@@ -25,7 +25,7 @@ import java.util.List;
 @Entity
 @Table(name = "TB_PRODUTO")
 @Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "P",
+@DiscriminatorColumn(name = "DISC_PRODUTO",
         discriminatorType = DiscriminatorType.STRING, length = 1)
 public abstract class Produto {
 
